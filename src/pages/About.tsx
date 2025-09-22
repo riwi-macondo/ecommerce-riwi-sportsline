57aa4c23ce347249843bd37b496620c7fa1c498e
ocamachop <ocamacho@infortributos.com>
2025-09-22 07:45:30 -0500

    feat:configuracion del router

---
+++ src/pages/About.tsx
@@ -0,0 +1,10 @@
+function About(){
+    return(
+        <div className="bg-red-200 p-4">
+            <h1>About Us</h1>
+            <p>This page contains information about our application.</p>
+        </div>
+    )
+}
+
+export default About
