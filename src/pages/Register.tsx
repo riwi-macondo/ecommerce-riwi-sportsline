b5da5759342cf451ae651c4a4e1af480c3c5c3f8
Moisés Cantillo <109709947+moicanbas@users.noreply.github.com>
2025-09-23 10:54:43 -0500

    feat: add primereact UI
    - add primereact
    - dark and light mode configuration
    - basic colors

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,7 +1,31 @@
-function Register(){
-    return(
-        <div>
-            <h1>Register Page</h1>
+import { useState } from "react";
+import ThemeToggle from "../components/ThemeToggle"
+import { FloatLabel } from 'primereact/floatlabel';
+import { InputText } from "primereact/inputtext";
+
+
+function Register() {
+    const [username, setUsername] = useState<string>('');
+    const [email, setEmail] = useState<string>('');
+    return (
+        <div className="grid p-4 place-items-center w-full h-screen">
+            <div className="items-center p-4 bg-blue-300 rounded shadow shadow-xl w-96 h-96">
+                <div className="justify-self-end">
+                    <ThemeToggle />
+                </div>
+                <div className="container-form p-5 flex flex-row">
+                    <form className="space-y-7 grid grid-cols-1 w-full">
+                        <FloatLabel>
+                            <InputText className="w-full" id="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+                            <label htmlFor="username">Username</label>
+                        </FloatLabel>
+                        <FloatLabel>
+                            <InputText className="w-full" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                            <label htmlFor="email">Email</label>
+                        </FloatLabel>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
