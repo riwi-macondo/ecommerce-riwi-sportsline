56ad51472306fbaba268bbb1f5ee610cf735766b
Camilo Parra <camiloandres02222@gmail.com>
2025-09-25 08:48:37 -0500

    feat:agregando el inerceptor para los errores 401 y 403

---
+++ src/service/api.ts
@@ -0,0 +1,55 @@
+import axios, { AxiosError, AxiosResponse, type InternalAxiosRequestConfig } from "axios";
+
+
+
+
+const api = axios.create({
+    baseURL: '',
+    timeout: 5000
+});
+
+api.interceptors.request.use(
+    async (config: InternalAxiosRequestConfig) => {
+        try {
+            // Busco el tonken en le localStorage 
+            const auhtToken = localStorage.getItem('access_token')
+            // si hay un token lo hagrego al headers de la peticion
+            if (auhtToken) {
+                config.headers.Authorization = `Bearer ${auhtToken}`
+            }
+            return config
+
+        } catch (error) {
+            console.error('Error en interceptor de request:', error);
+            throw error
+        }
+    })
+
+
+api.interceptors.response.use(
+
+    async(response: AxiosResponse): Promise<AxiosResponse> => {
+    try {
+        return response;
+    } catch(error) {
+        
+
+        console.error('Error procesando la respuesta:', error);
+
+        
+        throw error;
+    }
+},
+
+    async (error: AxiosError) => {
+        if(error.response?.status === 401){
+            console.warn('No autorizado. Redirigiendo al login...');
+            window.location.href = '/login';
+        }
+
+        if(error.response?.status === 403){
+            console.warn('Acceso prohibido (403)');
+        }
+        return Promise.reject(error);
+    })
+
